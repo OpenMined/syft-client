c0458ca1f67d70f9d08e7a6462ff0701a17f58cf
rasswanth-s <43314053+rasswanth-s@users.noreply.github.com>
2025-10-08 20:17:23 +0530

    bump version 0.1.71

--- syft_client/__init__.py
+++ syft_client/__init__.py
@@ -13,7 +13,7 @@ reset_wallet = SyftClient.reset_wallet_static
 # Resolve Syft Paths
 from .syft_client import resolve_path
 
-__version__ = "0.1.70"
+__version__ = "0.1.71"
 
 __all__ = [
     "login",
