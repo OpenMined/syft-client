47c093df7fd661a08108e083f5bb53747757104f
eelcovdw <eelcovdw@gmail.com>
2025-10-10 15:56:08 +0200

    add comments + todos

--- syft_client/platforms/google_org/gmail.py
+++ syft_client/platforms/google_org/gmail.py
@@ -5,6 +5,7 @@ _DEFAULT_PLATFORM_NAME = "google_org"
 
 class GmailTransport(GmailTransportBase):
     def _get_platform_name(self) -> str:
+        # TODO remove dynamic attrs and use a normal typed attribute for _platform_client, _platform_client.platform
         _platform_client = getattr(self, "_platform_client", None)
         if _platform_client is None:
             return _DEFAULT_PLATFORM_NAME
