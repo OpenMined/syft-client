31d2c72e79f46b4322873a407622fe90c6737d1a
rasswanth-s <43314053+rasswanth-s@users.noreply.github.com>
2025-11-03 20:42:03 +0530

    update dataset managed with syftbox config

---
+++ packages/syft-datasets/src/syft_datasets/config.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel, Field
+from pathlib import Path
+
+class SyftBoxConfig(BaseModel):
+    syftbox_folder: Path = Field(..., description="Path to the SyftBox folder on the local filesystem.")
+    email: str = Field(..., description="Email associated with the SyftBox.")
+
+    @property
+    def datasites(self) -> Path:
+        return self.syftbox_folder / "datasites"
+    
+    @property
+    def private_dir(self) -> Path:
+        return self.syftbox_folder / "private"
+
+    @property
+    def public_dir(self) -> Path:
+        return self.datasites / self.email / "public"
+    
