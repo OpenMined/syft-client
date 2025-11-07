011099ac5d366159640dee47c32c35b0e8a13ca5
Koen van der Veen <koenlennartvanderveen@gmail.com>
2025-11-07 14:51:57 +0100

    -

--- syft_client/sync/syftbox_manager.py
+++ syft_client/sync/syftbox_manager.py
@@ -81,6 +81,7 @@ class SyftboxManagerConfig(BaseModel):
             raise ValueError(
                 "At least one of only_ds or only_datasite_owner must be True"
             )
+        print("email", email)
 
         syftbox_folder = get_colab_default_syftbox_folder(email)
         use_in_memory_cache = False
