ef6f93843ebb296ac4a645a065e4db402b87c2b4
Koen van der Veen <koenlennartvanderveen@gmail.com>
2026-03-19 13:07:26 +0100

    verbose

--- packages/syft-datasets/src/syft_datasets/__init__.py
+++ packages/syft-datasets/src/syft_datasets/__init__.py
@@ -1,4 +1,4 @@
 from .config import SyftBoxConfig
 
-__version__ = "0.1.5"
+__version__ = "0.1.7"
 __all__ = ["SyftBoxConfig"]
--- packages/syft-job/src/syft_job/__init__.py
+++ packages/syft-job/src/syft_job/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.1.23"
+__version__ = "0.1.24"
 
 from .client import JobClient, get_client
 from .config import SyftJobConfig
